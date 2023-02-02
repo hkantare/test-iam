08898875a718b56dd3cfc34f5ce61df133f4ac61
HARINI KANTAREDDY <hkantare@in.ibm.com>
2023-02-02 10:21:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,19 @@ terraform {
       source = "IBM-Cloud/ibm"
       version = "1.50.0"
     }
+     http = {
+      source = "hashicorp/http"
+      version = "3.2.1"
+    }
+  }
+}
+
+
+data "http" "example" {
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
   }
 }
